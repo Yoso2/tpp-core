b9c8332723e1c0b41b0ffb6931f6fdc432866dd9
Felk <de.felix.koenig@gmail.com>
2021-01-07 19:12:35 +0100

    add InputConfig to RunmodeConfig

--- Core/Configuration/RunmodeConfig.cs
+++ Core/Configuration/RunmodeConfig.cs
@@ -3,5 +3,7 @@ namespace Core.Configuration
     public class RunmodeConfig : ConfigBase, IRootConfig
     {
         public string Schema => "./config.runmode.schema.json";
+
+        public InputConfig InputConfig { get; init; } = new();
     }
 }
